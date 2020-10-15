aad2f8230a84931e524a5147f42a6a5f64e576c4
vidurshan <vidurshan1996>
2020-10-15 13:15:55 +0530

    menu for conductor

--- app/src/main/java/com/example/eticket_admin/scan_ticket.java
+++ app/src/main/java/com/example/eticket_admin/scan_ticket.java
@@ -1,14 +1,89 @@
 package com.example.eticket_admin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 public class scan_ticket extends AppCompatActivity {
-
+    CodeScanner codeScanner;
+    CodeScannerView scannView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_ticket);
+        scannView = findViewById(R.id.scannerView);
+        codeScanner = new CodeScanner(this,scannView);
+
+        codeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        //  resultData.setText(result.getText());
+                        String data1 = result.getText();
+                        Intent intent1 = new Intent(getApplicationContext(),confirm_ticket.class);
+                        intent1.putExtra("scandata",data1);
+                        startActivity(intent1);
+                    }
+                });
+
+            }
+        });
+
+        scannView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                codeScanner.startPreview();
+            }
+        });
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        requestForCamera();
+
+    }
+
+    public void requestForCamera() {
+        Dexter.withActivity(this).withPermission(Manifest.permission.CAMERA).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse response) {
+                codeScanner.startPreview();
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse response) {
+                Toast.makeText(scan_ticket.this, "Camera Permission is Required.", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                token.continuePermissionRequest();
+
+            }
+        }).check();
+    }
+
+
+
 }
+
