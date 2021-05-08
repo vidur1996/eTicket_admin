2382b02cd0eb7a59a99fae8e231a4b70a3661f47
vidurshan <vidurshan1996>
2021-05-08 23:46:11 +0530

    designed and implemented topup

--- app/src/main/java/com/example/eticket_admin/signup.java
+++ app/src/main/java/com/example/eticket_admin/signup.java
@@ -1,7 +1,8 @@
 package com.example.eticket_admin;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -13,47 +14,64 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.eticket_admin.data.Admin;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class signup extends AppCompatActivity {
-    EditText password_edit, editText_email  ;
+    EditText password_edit, editText_email, editText_name, editText_phone, editText_username;
     EditText confirm_password_edit;
-    TextView password_error,email_error ;
-    Button signup ;
+    TextView password_error, email_error;
+    Button signup;
     String password;
     Spinner spinner_userType;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
         spinner_userType = findViewById(R.id.spinner_userType);
-         password_edit  = findViewById(R.id.password_edit);
-         confirm_password_edit=findViewById(R.id.password_confirm_edit);
+        password_edit = findViewById(R.id.password_edit);
+        confirm_password_edit = findViewById(R.id.password_confirm_edit);
         password_error = findViewById(R.id.text_password_invalid);
         signup = findViewById(R.id.button_signup);
         editText_email = findViewById(R.id.editText_email);
         email_error = findViewById(R.id.text_email_error);
+        editText_name = findViewById(R.id.editText_name);
+        editText_phone = findViewById(R.id.editText_phone);
+        editText_username = findViewById(R.id.editText_username);
 
-        ArrayAdapter<String> myadapter  = new ArrayAdapter<String>(signup.this
-                ,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.user_type));
+        ArrayAdapter<String> myadapter = new ArrayAdapter<String>(signup.this
+                , android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.user_type));
         myadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner_userType.setAdapter(myadapter);
 
 
         editText_email.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if (editText_email.getText().toString().matches("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+")){
-                    email_error.setText("invalid email");
-                }else{
+                if (editText_email.getText().toString().matches("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+")) {
                     email_error.setText("");
+                } else {
+
+                    email_error.setText("invalid email");
                 }
 
             }
 
             @Override
-            public void afterTextChanged(Editable s) { }
+            public void afterTextChanged(Editable s) {
+            }
         });
 
         signup.setOnClickListener(new View.OnClickListener() {
@@ -62,41 +80,92 @@ public class signup extends AppCompatActivity {
                 String utype = spinner_userType.getSelectedItem().toString();
                 String pass1 = password_edit.getText().toString().trim();
                 String pass2 = confirm_password_edit.getText().toString().trim();
-                if(comparePassword(pass1,pass2)){
-                    Toast.makeText(signup.this,"valid password",Toast.LENGTH_LONG).show();
+                if (comparePassword(pass1, pass2) && verifyData()) {
+                    if (saveData(utype, password)) {
+                        Toast.makeText(signup.this, "registration successful", Toast.LENGTH_LONG).show();
+                        showAlert();
+                    }
+
                 }
             }
         });
     }
 
 
+    public void showAlert() {
+        new MaterialAlertDialogBuilder(signup.this)
+                .setTitle("Registration success")
+                .setMessage("You will recieve a mail after verification")
+                .setPositiveButton("GOT IT", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Intent it = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(it);
+                        finish();
 
-    public boolean comparePassword(String pass1,String pass2)
-    {
+                    }
+                }).show();
+
+    }
 
 
-
-        if(pass1.equals(pass2)){
-            this.password = pass1;
-            password_error.setText("");
-            return true;
-        }else {
+    public boolean comparePassword(String pass1, String pass2) {
+        if (pass1.equals(pass2)) {
+            if (pass1 != "" && pass2 != "") {
+                this.password = pass1;
+                password_error.setText("");
+                return true;
+            } else {
+                return false;
+            }
+        } else {
             password_error.setText("Both password dont match");
-            return   false;
+            return false;
         }
     }
 
-    public Boolean saveData(String utype,String name,String email,String pNo,String username,String password){
-        return true;
+    public Boolean saveData(String utype, String password) {
+        Admin admin = new Admin();
+        admin.setName(editText_name.getText().toString().trim());
+        admin.setEmail(editText_email.getText().toString().trim());
+        admin.setNum(editText_phone.getText().toString().trim());
+        admin.setUsername(editText_username.getText().toString().trim());
+        admin.setPassword(password);
 
+        DatabaseReference reff = FirebaseDatabase.getInstance().getReference().child("admin_pending").child(utype);
+        final boolean[] check = {true};
+        DatabaseReference.CompletionListener completionListener = new DatabaseReference.CompletionListener() {
+            @Override
+            public void onComplete(@Nullable DatabaseError error, @NonNull DatabaseReference ref) {
+                check[0] = error == null;
+            }
+        };
+        reff.child(editText_username.getText().toString().trim()).setValue(admin, completionListener);
+
+        return check[0];
     }
 
-    public Boolean verifyData(){
-        if
+    public Boolean verifyData() {
+        if (editText_username.getText().toString().equals("")) {
+            Toast.makeText(signup.this, "username required", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if (editText_name.getText().toString().equals("")) {
+            Toast.makeText(signup.this, "name required", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if (editText_email.getText().toString().equals("")) {
+            Toast.makeText(signup.this, "email required", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if (editText_phone.getText().toString().equals("")) {
+            Toast.makeText(signup.this, "phone number required", Toast.LENGTH_LONG).show();
+            return false;
+        } else {
+            return true;
+        }
 
     }
-
-
 
 
 }
--- app/src/main/java/com/example/eticket_admin/topup/menu_topup.java
+++ app/src/main/java/com/example/eticket_admin/topup/menu_topup.java
@@ -1,35 +1,159 @@
 package com.example.eticket_admin.topup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.eticket_admin.MainActivity;
 import com.example.eticket_admin.R;
+import com.example.eticket_admin.signup;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class menu_topup extends AppCompatActivity {
-    Button signout;
+    EditText editText_username,editText_amount;
+    TextView username_error;
+    Button signout,clear,pay,change_pass;
+    String topname,pastvalue,amount;
+    Boolean check = false;
+
+    DatabaseReference topup_reffer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_topup);
-
-
+        editText_amount = findViewById(R.id.amount_txt);
+        editText_username=  findViewById(R.id.editText_username_topup);
+        username_error = findViewById(R.id.text_username_invalid_topup);
+        clear=findViewById(R.id.clear_pay_btn);
+        pay=findViewById(R.id.confirm_pay_btn);
+        change_pass=findViewById(R.id.change_password_topup);
         signout  = findViewById(R.id.out_btn3);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null)
+        {
+            topname = extras.getString("topname");
+        }
 
         signout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent in1 = new Intent(getApplicationContext(), MainActivity.class);
-
-
                 startActivity(in1);
                 menu_topup.this.finish();
             }
         });
+        change_pass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in1 = new Intent(getApplicationContext(),TopUpChangePasswordActivity.class);
+                in1.putExtra("topname",topname);
+                startActivity(in1);
+                menu_topup.this.finish();
+            }
+        });
+
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText_amount.setText("");
+                editText_username.setText("");
+            }
+        });
+
+        pay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(check_value()){
+                    if(topup()){
+                                showAlert();
+                    }
+
+                }
+            }
+        });
+
+    }
+    public boolean check_value(){
+        if(editText_username.getText().toString()!=""){
+            username_error.setText("");
+            if(editText_amount.getText().toString()!=""){
+                return true;
+
+            }else {
+                Toast.makeText(menu_topup.this, "enter amount", Toast.LENGTH_LONG).show();
+                return false;
+            }
+        }
+        else{
+            username_error.setText("username is empty");
+            return false;
+        }
+    }
+
+    public boolean topup(){
+        String username = editText_username.getText().toString().trim();
+        amount = editText_amount.getText().toString().trim();
+        topup_reffer = FirebaseDatabase.getInstance().getReference().child("member").child(username);
+
+
+
+        topup_reffer.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                pastvalue = snapshot.child("balance").getValue().toString().trim();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        int newvalue=0;
+    if(pastvalue!=null && amount !=null){
+        newvalue = Integer.parseInt(pastvalue)+Integer.parseInt(amount);
+    }
+
+
+        DatabaseReference.CompletionListener completionListener = new DatabaseReference.CompletionListener() {
+            @Override
+            public void onComplete(@Nullable DatabaseError error, @NonNull DatabaseReference ref) {
+                check = true;
+            }
+        };
+        topup_reffer.child("balance").setValue(newvalue, completionListener);
+
+            return check;
+
+    }
+
+    public void showAlert() {
+        new MaterialAlertDialogBuilder(menu_topup.this)
+                .setTitle("Top-up success")
+                .setMessage("You have top-up Rs."+amount)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        editText_amount.setText("");
+                        editText_username.setText("");
+                        check = false;
+
+                    }
+                }).show();
 
     }
 }
